25a3a5b706ee7f139d61e16dbabacfe863be8c94
priyanshi1933 <pd.opash@gmail.com>
2026-03-25 18:03:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+gsap.to("#box1",{
+    x:1200,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:2,
+    repeat:1
+})
+gsap.from("#box2",{
+    x:1200,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:2,
+    repeat:1
+})
+gsap.from("h1",{
+    opacity:0,
+    y:30,
+    duration:2,
+    delay:1,
+    stagger:0.3
+})
