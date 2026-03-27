5cfe19c3f744b4a1205dc8d44e1ebdd56fbf6b0e
priyanshi1933 <pd.opash@gmail.com>
2026-03-27 18:02:10 +0530

    first commit

--- text.js
+++ text.js
@@ -1,8 +1,36 @@
-var h1Text=document.querySelector("h1").textContent
-var splittedText=h1Text.split("")
-var clutter=""
-splittedText.forEach(function(){
-   clutter=clutter+"Hey"
+function breakTheText() {
+  var h1 = document.querySelector("h1");
+  var h1Text = h1.textContent;
+
+  var splittedText = h1Text.split("");
+  var halfValue=splittedText.length/2;
+
+  var clutter="";
+
+  splittedText.forEach(function (e,idx) {
+      if(idx<halfValue){
+         clutter+=`<span class="a">${e}</span>`
+      }else{
+         clutter+=`<span class="b">${e}</span>`
+      }
+  });
+  h1.innerHTML=clutter
+}
+
+breakTheText();
+
+gsap.from("h1 .a",{
+   y:80,
+   duration:0.6,
+   delay:0.5,
+   stagger:0.15,
+   opacity:0
 })
-console.log(clutter);
+gsap.from("h1 .b",{
+   y:80,
+   duration:0.6,
+   delay:0.5,
+   stagger:-0.15,
+   opacity:0
+})
 
